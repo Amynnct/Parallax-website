e81a8dfbad633c62e00166641a149fca4a9f489c
Amynnct <61101860+Amynnct@users.noreply.github.com>
2024-11-05 23:47:26 -0500

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { RouterModule } from '@angular/router';
+import { ClockComponent } from './clock/clock.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ClockComponent
+  ],
+  imports: [
+    BrowserModule,
+    YouTubePlayerModule,
+    RouterModule.forRoot([
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
